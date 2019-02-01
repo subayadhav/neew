2ece0dd51fcfe19ffc5c12d662783b4e7225d0b9
subayadhav <suba10499@gmail.com>
2019-02-01 23:16:59 +0530

    Create Fibanacci.java

---
+++ Fibanacci.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+ public class Fibanacci{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Fibanacci Range:");
+    int range=sc.nextInt(System.in);
+    int temp=0;
+    for(int i=0;i<range;i++);
+    {
+    temp=temp+i;
+    System.out.println(temp);
+    }
+   }
+} 
