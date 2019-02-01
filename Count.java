7b98e1a156ca4f40c736d705f120e2807cabc750
subayadhav <suba10499@gmail.com>
2019-02-01 22:59:38 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Count{
+      public static void main(String args[]){
+      //Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Your Strinf...");
+      String Input=laptop is good;
+      int c=0;
+      for(int i=0;i<Input.length();i++)
+      {
+        if(Input.charAt(i)!=' '){
+        c++;
+        }
+      }
+      System.out.println("The Count is "+c);
+     }
+} 
