aa5d5a1209371e015fa71bb05f27b08b417b567a
subayadhav <suba10499@gmail.com>
2019-02-01 22:24:14 +0530

    Create Mini.java

---
+++ Mini.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class  Mini
+{
+public static void main(String args[])
+{
+int min;
+int a[]={1,4,5,6,22};
+min=a[0];
+System.out.println("Minimum element");
+for(int i=0;i<a.length;i++)
+{
+int j=i+1;
+if(j<a.length)
+{
+if(a[i]>a[j])
+{
+min=a[j];
+}
+}
+}
+System.out.println(min);
+}
+}
