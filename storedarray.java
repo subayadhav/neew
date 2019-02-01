da42ee4ff3968f9d2fa3b40d8e6b1d773590421f
subayadhav <suba10499@gmail.com>
2019-02-01 22:22:28 +0530

    Create storedarray.java

---
+++ storedarray.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+public class storedarray
+{
+public static void main(String args[])
+{
+ Scanner sc=new Scanner(System.in)
+  int temp;
+ int n=sc.nextInt();
+ int a[]=new int[n];
+ for(int i=0;i<n;i++)
+ {
+  a[i]=sc.nextInt();
+ }
+ for(i=0;i<n;i++)
+ {
+  for(int j=i+1;j<n;j++)
+  {
+   if(a[i]>a[j])
+   {
+    temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+   }
+  }
+ }
+ for(int i=0;i<n;i++)
+ {
+  System.out.print(a[i]+"");
+ }
+}
+}
+   
